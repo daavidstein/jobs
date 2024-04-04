835e33702f47babfde1147f8cb6d8ab40a5772bc
Daavid Stein <steindaavid@gmail.com>
2024-04-04 13:52:57 -0400

    ddg_search/update_image.sh

--- aws_lambda/ddg_search/update_image.sh
+++ aws_lambda/ddg_search/update_image.sh
@@ -1,7 +1,1 @@
-docker build --platform linux/amd64 -t ddg-search:latest .
-docker tag ddg-search:latest 652060930823.dkr.ecr.us-east-1.amazonaws.com/ddg-search:latest
-aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin  652060930823.dkr.ecr.us-east-1.amazonaws.com
-docker push 652060930823.dkr.ecr.us-east-1.amazonaws.com/ddg-search:latest
-aws lambda update-function-code \
-    --function-name ddg-search \
-    --image-uri 652060930823.dkr.ecr.us-east-1.amazonaws.com/ddg-search:latest
+../update_lambda.sh ddg-search
